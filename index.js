7da23b475cdb6c2794e75fea92b91c195a34b440
seungkyu2 <zx6658@naver.com>
2018-03-19 12:40:12 +0900

    add env file and edit structure

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const cron = require('./cron');
+require('./config/env');
 
+//크론이 시작되면 할당된 작업들을 실행해준다.
 cron.init((jobs) => {
-  console.log('console init');
+  console.log('cron init');
+  console.log(process.env.NODE_ENV);
+  console.log(process.env.PORT);
 });
