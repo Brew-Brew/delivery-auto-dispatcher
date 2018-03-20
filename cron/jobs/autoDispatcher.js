07ab0cf32d8fa13b31c8b3e5363c6349c9c61d30
seungkyu2 <zx6658@naver.com>
2018-03-20 12:05:07 +0900

    edit log message and add check function

--- cron/jobs/autoDispatcher.js
+++ cron/jobs/autoDispatcher.js
@@ -11,7 +11,7 @@ const {
 module.exports = () => {
   const cron = new Cron();
   cron.schedule(jobName, {
-    second: '*/10', // for test
+    second: '*/5', // for test
     // hour: '12', // utc 12 is kst 21
     // minute: '20,30',
   }, async () => {
@@ -28,19 +28,28 @@ module.exports = () => {
             },
         },
       };
+      //주문관련 정보를 받아온다.
       const orders = await orderMeta.findAll(orderOptions);
-      orders.forEach(async(od)=>{
+      if(orders.length === 0){
+        console.log('모두 배송접수 되었습니다.!');
+      }
+
+      //각 주문에 해당하는 timeslot정보를 가져온다.
+      orders.forEach(async(od) => {
+        console.log('-------주문관련정보-----------')
         console.log('주문번호',od.id);
         console.log('timeslot id: ',od.timeSlotId);
-        //긱 주문에 해당하는 timeslot정보 가져옴.
+        //order의 timeslot id와 timeslot의 id 같은것 가져옴
         const timeslot = await timeSlot.findOne({
           where: {
           id: od.timeSlotId,
         }
       });
+
+      console.log('-------배송관련정보-----------')
       //배달원하는시간과 현재시간을 체크하여 부릉 api에 접수해준다.
       // if( timeslot.timeStr- 현재시간 <=1시간) -> 접수
-
+      // 날짜도 체크해야한다.
       //현재시간관련 즉시 실행함수
       var nowTime = (function () {
         var now = moment().format("HH:mm");
@@ -63,11 +72,31 @@ module.exports = () => {
       }());
       console.log('배송원하는 시간:',timeslot.timeStr);
 
+
       var hour_gap= Math.floor((deliverTime.getTime() - nowTime.getTime())/1000/60/60);
       var min_gap= Math.round(((deliverTime.getTime() - nowTime.getTime())/1000/60)-hour_gap*60);
-      console.log('배송까지 약 ' + hour_gap + '시간 '+min_gap+'분 남았습니다.');
-      console.log('--------- 배송 접수 현황을 다시 체크중입니다. ----------------');
-      });
+
+      //남은 시간 양수여야 실행한다.
+      if(hour_gap >= 0 && min_gap >0){
+        console.log('배송까지 약 ' + hour_gap + '시간 '+min_gap+'분 남았습니다.');
+        if( hour_gap <=1 ){
+        //접수 프로세스
+        // await Promise.all(
+        //   orders.map(order =>
+        //     order.update({
+        //       delivery_type: 'vroong',
+        //       },
+        //       { where: {id: od.id}},
+        //     )
+        //   )
+        // );
+        console.log(`배송까지 남은시간 <= 1시간 이므로 주문번호: ${od.id}를 접수하였습니다.`);
+        }
+      }
+      else{
+        console.log('배송을 원하는시간이 지났습니다!')
+      }
+    });
 
 
 
